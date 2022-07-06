5ef65c08ea1e04742961f5c3443c81fb02606e7a
Long Ng <long26022000@gmail.com>
2022-07-07 06:20:00 +0700

    update start screen

--- lib/screens/StartPlan.dart
+++ lib/screens/StartPlan.dart
@@ -1,5 +1,12 @@
+import 'package:beefit/constants/AppStyles.dart';
+import 'package:beefit/widgets/CommonButton.dart';
+import 'package:beefit/widgets/CountDownTimerState.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:video_player/video_player.dart';
+import '../constants/AppMethods.dart';
+import '../widgets/TimeLine.dart';
 
 class StartPlan extends StatefulWidget {
   const StartPlan({Key? key}) : super(key: key);
@@ -9,64 +16,313 @@ class StartPlan extends StatefulWidget {
 }
 
 class _StartPlanState extends State<StartPlan> {
-  late VideoPlayerController controller;
+  // late VideoPlayerController controller;
+  final CountDownTimerState timerState = Get.put(CountDownTimerState());
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    controller = VideoPlayerController.network("assets/video.mp4")
-      ..addListener(() {})
-      ..setLooping(true)
-      ..initialize().then((_) => controller.play());
+    // controller = VideoPlayerController.network("assets/video.mp4")
+    //   ..addListener(() {})
+    //   ..setLooping(true)
+    //   ..initialize().then((_) => controller.play());
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
-    controller.dispose();
+    // controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final _scaleFont = AppMethods.fontScale(context);
+    final _scaleScreen = AppMethods.screenScale(context);
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Padding(
+                //   padding: const EdgeInsets.symmetric(vertical: 8),
+                //   child: ClipRRect(
+                //       borderRadius: BorderRadius.circular(15),
+                //       // child: VideoPlayer(
+                //       //   controller: controller,
+                //       // )),
+                //       child: Image.network(
+                //           'https://c.tenor.com/h9W4zejLjTMAAAAC/fit-workout.gif')),
+                // ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10 * _scaleScreen),
+                  child: Image.network(
+                      'https://c.tenor.com/h9W4zejLjTMAAAAC/fit-workout.gif'),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10 * _scaleScreen),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Push up'.toUpperCase(),
+                        style: GoogleFonts.poppins(
+                          color: AppStyle.secondaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30 * _scaleFont,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () => showModalBottomSheet(
+                            context: context,
+                            isScrollControlled: true,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(20)),
+                            ),
+                            builder: (context) => buildSheet()),
+                        icon: const Icon(
+                          Icons.help,
+                          color: AppStyle.gray5Color,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10 * _scaleScreen),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10 * _scaleScreen),
+                        child: IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {},
+                          icon: Center(
+                            child: Icon(
+                              Icons.navigate_before,
+                              size: 50 * _scaleFont,
+                              color: AppStyle.secondaryColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 80 * _scaleScreen,
+                        width: 80 * _scaleScreen,
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            GetBuilder<CountDownTimerState>(
+                              builder: (_) => CircularProgressIndicator(
+                                value: timerState.count / timerState.max,
+                                strokeWidth: 08,
+                                color: AppStyle.secondaryColor,
+                                backgroundColor: AppStyle.gray5Color,
+                              ),
+                            ),
+                            GetBuilder<CountDownTimerState>(
+                              builder: (_) => Center(
+                                child: !timerState.isStart
+                                    ? SizedBox(
+                                        width: double.infinity,
+                                        height: double.infinity,
+                                        child: ElevatedButton(
+                                          onPressed: () {
+                                            timerState.stateTimerStart();
+                                          },
+                                          child: Center(
+                                            child: Icon(
+                                              Icons.play_arrow,
+                                              size: 50 * _scaleFont,
+                                            ),
+                                          ),
+                                          style: ElevatedButton.styleFrom(
+                                            padding: EdgeInsets.zero,
+                                            elevation: 0,
+                                            shape: const CircleBorder(
+                                              side: BorderSide.none,
+                                            ),
+                                            primary:AppStyle.primaryColor,
+                                          ),
+                                        ),
+                                      )
+                                    : Text(
+                                        '${timerState.count}',
+                                        style: GoogleFonts.poppins(
+                                          fontSize: 30 * _scaleFont,
+                                          fontWeight: FontWeight.bold,
+                                          color: AppStyle.secondaryColor,
+                                        ),
+                                      ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10 * _scaleScreen),
+                        child: IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {},
+                          icon: Center(
+                            child: Icon(
+                              Icons.navigate_next,
+                              size: 50 * _scaleFont,
+                              color: AppStyle.secondaryColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // Row(
+                //   children: [
+                //     ElevatedButton(
+                //       onPressed: () {
+                //         timerState.pause();
+                //       },
+                //       child: const Text('Pause'),
+                //     ),
+                //     ElevatedButton(
+                //       onPressed: () {
+                //         timerState.reset();
+                //       },
+                //       child: const Text('Reset'),
+                //     ),
+                //   ],
+                // ),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: VideoPlayer(
-                      controller: controller,
-                    )),
+    );
+  }
+
+  Widget buildSheet() {
+    return DraggableScrollableSheet(
+      initialChildSize: 1,
+      minChildSize: 0.5,
+      builder: (_, scrollController) => Container(
+        padding: const EdgeInsets.fromLTRB(30, 20, 30, 30),
+        child: ListView(
+          controller: scrollController,
+          shrinkWrap: true,
+          primary: false,
+          padding: EdgeInsets.zero,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                margin: const EdgeInsets.only(bottom: 8),
+                width: 80,
+                height: 4,
+                decoration: const BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
               ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Icon(
-                    Icons.play_arrow,
-                    size: 50,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  shape: const CircleBorder(
-                    side: BorderSide.none,
-                  ),
-                  primary: const Color(0xffE4A248),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                    'assets/imgs/fitness1.png',
+                  width: double.infinity,
+                  // height: 160,
+                  fit: BoxFit.cover,
                 ),
               ),
-            ],
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Text(
+                'Push up',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Text(
+                'Performing hot reload...Syncing files to device Android SDK built for x86...Reloaded 1 of 607 libraries in 687ms.',
+                style: TextStyle(color: Colors.grey, fontSize: 14),
+              ),
+            ),
+            Timeline(
+              children: const <Widget>[
+                SizedBox(
+                  height: 50,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+              ],
+              indicators: const <Widget>[
+                NodeCircle(),
+                NodeCircle(),
+                NodeCircle(),
+                NodeCircle(),
+              ],
+              lineColor: const Color(0xffE4A248),
+            ),
+            CommonButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              backgroundColor: const Color(0xffE4A248),
+              text: 'Close'.toUpperCase(),
+              textColor: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NodeCircle extends StatelessWidget {
+  const NodeCircle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: Container(
+        padding: const EdgeInsets.all(5),
+        height: 22,
+        width: 22,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: const Color(0xffE4A248),
+            width: 1,
+          ),
+        ),
+        child: Container(
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+              colors: [Color(0xffE9A24A), Color(0xffF2BE6A), Color(0xffF6D08B)],
+              end: Alignment.bottomRight,
+              begin: Alignment.topLeft,
+            ),
           ),
         ),
       ),
@@ -76,6 +332,7 @@ class _StartPlanState extends State<StartPlan> {
 
 class VideoPlayer extends StatelessWidget {
   VideoPlayerController _controller;
+
   VideoPlayer({required VideoPlayerController controller, Key? key})
       : _controller = controller,
         super(key: key);
---
+++ lib/widgets/CountDownTimerState.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'package:get/get.dart';
+
+class CountDownTimerState extends GetxController {
+  var max = 30, count = 30;
+  bool isStart = false, isPause = false, isReset = false;
+  late Timer _timer;
+
+  void stateTimerStart() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (count > 0) {
+        count--;
+        isPause = false;
+        isStart = true;
+        isReset = false;
+        update();
+      } else {
+        _timer.cancel();
+      }
+    });
+  }
+
+  // user can set count down seconds, from TextField
+  void setNumber(var number) {
+    count = int.parse(number);
+    update();
+  }
+
+  // pause the timer
+  void pause() {
+    _timer.cancel();
+    isPause = true;
+    isStart = false;
+    isReset = false;
+    update();
+  }
+
+  // reset count value to 10
+  void reset() {
+    _timer.cancel();
+    isPause = false;
+    isStart = false;
+    isReset = true;
+    count = 30;
+    update();
+  }
+}
